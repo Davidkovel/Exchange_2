b84f5bfdd8b7b06c826d582a2d390489ce95a6a8
David <dkovel7@gmail.com>
2025-07-07 23:58:31 +0100

    Changed postgres url

--- src/database/db.py
+++ src/database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, Asyn
 
 from src.database.base import Base
 
-url = f"postgresql+asyncpg://postgres:mysecretpassword@localhost:5432/postgres"
+url = f"postgresql+asyncpg://postgres:mysecretpassword@postgres:5432/postgres"
 engine = create_async_engine(url=url, echo=False, future=True)
 db_pool = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
 
